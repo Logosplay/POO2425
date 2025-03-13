c9ec9dd18895c536407cdad0d257d6d33c54739c
91Ivo <goncalo.ivo.2005@gmail.com>
2025-03-13 12:24:16 +0000

    Add files via upload

---
+++ ContactBook.java
@@ -0,0 +1,191 @@
+/**
+ * The ContactBook class represents a collection of contacts.
+ */
+public class ContactBook {
+    static final int DEFAULT_SIZE = 100;
+
+    private int counter;
+    private Contact[] contacts;
+
+    /**
+     * Constructs a ContactBook with a default size and initializes the counter.
+     */
+    public ContactBook() {
+        counter = 0;
+        contacts = new Contact[DEFAULT_SIZE];
+    }
+
+    /**
+     * Checks if a contact with the given name exists in the contact book.
+     *
+     * @param name the name of the contact to check
+     * @return true if the contact exists, false otherwise
+     */
+    public boolean hasContact(String name) {
+        return searchIndex(name) >= 0;
+    }
+
+    public boolean hasPhone(int phone) {
+        return searchPhone (phone) >= 0;
+    }
+
+    public boolean sharesPhone() {
+        return searchShared ();
+    }
+
+    /**
+     * Gets the number of contacts in the contact book.
+     *
+     * @return the number of contacts
+     */
+    public int getNumberOfContacts() {
+        return counter;
+
+    }
+
+    /**
+     * Adds a new contact to the contact book.
+     *
+     * @param name  the name of the contact
+     * @param phone the phone number of the contact
+     * @param email the email address of the contact
+     * @pre !hasContact(name)
+     */
+    public void addContact(String name, int phone, String email) {
+        if (counter == contacts.length)
+            resize();
+        contacts[counter] = new Contact(name, phone, email);
+        counter++;
+    }
+
+    /**
+     * Deletes a contact from the contact book.
+     *
+     * @param name the name of the contact to delete
+     * @pre hasContact(name)
+     */
+    public void deleteContact(String name) {
+        int index = searchIndex(name);
+        for (int i = index; i < counter; i++)
+            contacts[i] = contacts[i + 1];
+        counter--;
+    }
+
+    /**
+     * Gets the phone number of a contact with the given name.
+     *
+     * @param name the name of the contact
+     * @return the phone number of the contact
+     * @pre hasContact(name)
+     */
+    public int getPhone(String name) {
+        return contacts[searchIndex(name)].getPhone();
+    }
+
+    public String getName(int phone) {
+        return contacts[searchPhone(phone)].getName();
+    }
+
+    /**
+     * Gets the email address of a contact with the given name.
+     *
+     * @param name the name of the contact
+     * @return the email address of the contact
+     * @pre hasContact(name)
+     */
+    public String getEmail(String name) {
+        return contacts[searchIndex(name)].getEmail();
+    }
+
+    /**
+     * Changes the phone number of a contact with the given name.
+     *
+     * @param name  the name of the contact
+     * @param phone the new phone number
+     * @pre hasContact(name)
+     */
+    public void setPhone(String name, int phone) {
+        contacts[searchIndex(name)].setPhone(phone);
+    }
+
+    /**
+     * Changes the email address of a contact with the given name.
+     *
+     * @param name  the name of the contact
+     * @param email the new email address
+     * @pre hasContact(name)
+     */
+    public void setEmail(String name, String email) {
+        contacts[searchIndex(name)].setEmail(email);
+    }
+
+    /**
+     * Returns an iterator for the contacts in the contact book.
+     *
+     * @return a ContactIterator for iterating over contacts
+     */
+    public ContactIterator iterator() {
+        return new ContactIterator(contacts, counter);
+    }
+
+    private int searchIndex(String name) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i < counter && !found)
+            if (contacts[i].getName().equals(name))
+                found = true;
+            else
+                i++;
+        if (found) result = i;
+        return result;
+    }
+
+    private int searchPhone(int phone) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i < counter && !found)
+            if (contacts[i].getPhone() == phone)
+                found = true;
+            else
+                i++;
+        if (found) result = i;
+        return result;
+    }
+
+    private boolean searchShared() {
+        int i = 1;
+        int j = 0;
+        boolean found = false;
+        
+        if (contacts[0] == null) {
+            return false;
+        }
+
+        int phone = contacts[0].getPhone();
+
+        while (!found && j < counter - 1){
+            if (i >= counter){
+                j++;
+                phone = contacts[j].getPhone();
+                i = j + 1;
+            }
+
+            if (contacts[i] != null && contacts[i].getPhone() == phone) {
+                found = true;
+            
+            } else {
+                i++;
+            }
+        }
+        return found;
+    }
+
+    private void resize() {
+        Contact tmp[] = new Contact[2 * contacts.length];
+        for (int i = 0; i < counter; i++)
+            tmp[i] = contacts[i];
+        contacts = tmp;
+    }
+}
---
+++ ContactIterator.java
@@ -0,0 +1,41 @@
+/**
+ * The ContactIterator class provides an iterator over an 
+ * array of Contact objects.
+ */
+public class ContactIterator {
+    private Contact[] contacts; // array of contacts
+    private int size; // number of contacts in the array
+    private int nextIndex; // index of the next contact
+
+    /**
+     * Constructs a ContactIterator with the specified 
+     * array of contacts and size.
+     *
+     * @param contacts the array of contacts to iterate over
+     * @param size the number of contacts in the array
+     */
+    public ContactIterator(Contact[] contacts, int size) {
+        this.contacts = contacts;
+        this.size = size;
+        nextIndex = 0;
+    }
+
+    /**
+     * Checks if there is a next contact in the iterator.
+     *
+     * @return true if there is a next contact, false otherwise
+     */
+    public boolean hasNext() {
+        return nextIndex < size;
+    }
+
+    /**
+     * Gets the next contact in the iterator.
+     *
+     * @return the next contact
+     * @pre hasNext()
+     */
+    public Contact next() {
+        return contacts[nextIndex++];
+    }
+}
